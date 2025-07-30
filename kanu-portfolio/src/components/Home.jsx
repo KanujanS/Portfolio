4041d1c401cb8ac4be2f5b22326df8e28adea21c
KanujanS <kanujansk@gmail.com>
2025-07-30 22:10:18 +0530

    Home page finished

--- kanu-portfolio/src/components/Home.jsx
+++ kanu-portfolio/src/components/Home.jsx
@@ -1,26 +1,49 @@
 import React from 'react'
 import PROFILE from '../assets/PROFILE.png'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import { BsBoxArrowUpRight } from "react-icons/bs";
+import { MdMailOutline } from "react-icons/md";
+import { FiGithub } from "react-icons/fi";
+import { FiLinkedin } from "react-icons/fi";
+import { FaMediumM } from "react-icons/fa";
 
 const Home = () => {
+    const [text] = useTypewriter({
+        words: ['Full-Stack Developer', 'MERN Enthusiast'],
+        loop: {},
+        typeSpeed: 130,
+        deleteSpeed: 80,
+    })
   return (
     <div className='flex mt-20'>
         <div>
             <p className='font-medium text-xl bg-gradient-to-r from-sky-400 via-sky-600 to-sky-800 bg-clip-text text-transparent'>I'm Kanujan Subakaran,</p>
-            <p className='text-white font-bold text-7xl py-8 text-shadow-xl'>Software <br /> <span className='bg-gradient-to-r from-sky-400 via-sky-600 to-sky-800 bg-clip-text text-transparent'>Engineer</span></p>
-            <p className='text-white text-2xl'>Full-Stack Developer</p>
-            <p className='text-lg text-gray-200 py-5'>Enthusiastic and dedicated aspiring Software Engineer with a solid understanding of full-stack development.</p>
-            <div className='text-white space-x-5'>
-                <button className='border px-5 py-2 rounded-3xl bg-gray-100/10'>MongoDB</button>
-                <button className='border px-5 py-2 rounded-3xl bg-gray-100/10'>Express.js</button>
-                <button className='border px-5 py-2 rounded-3xl bg-gray-100/10'>React.js</button>
-                <button className='border px-5 py-2 rounded-3xl bg-gray-100/10'>Node.js</button>
+            <p className='text-white font-bold text-7xl py-8' style={{ textShadow: '0px 0px 90px #0ea5e9' }}>Software <br /> <span className='bg-gradient-to-r from-sky-400 via-sky-600 to-sky-800 bg-clip-text text-transparent' >Engineer</span></p>
+            <p className='flex items-center'>
+                <span className='text-white text-2xl'>{text}</span>
+                <span className='text-sky-400 text-3xl animate-pulse ml-1'><Cursor cursorStyle='|'/></span>
+            </p>
+            <p className='text-lg text-gray-400 py-5'>Enthusiastic and dedicated aspiring Software Engineer with a solid understanding of full-stack development.</p>
+            <div className='text-white text-sm space-x-5'>
+                <button className='border border-gray-500 px-5 py-2 rounded-3xl bg-gray-100/10'>MongoDB</button>
+                <button className='border border-gray-500 px-5 py-2 rounded-3xl bg-gray-100/10'>Express.js</button>
+                <button className='border border-gray-500 px-5 py-2 rounded-3xl bg-gray-100/10'>React.js</button>
+                <button className='border border-gray-500 px-5 py-2 rounded-3xl bg-gray-100/10'>Node.js</button>
             </div>
-            <div className='flex gap-10 py-8 text-white'>
-                <p className='border border-none px-10 py-3 rounded-xl bg-black/50'>Projects</p>
-                <p className='border border-none px-10 py-3 rounded-xl bg-black/50'>Contact Me</p>
+            <div className='flex gap-10 py-8 text-white text-sm'>
+                <div className='w-45 justify-center gap-2 rounded-xl flex items-center px-7 py-3 bg-black/40' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}>
+                    <p>Projects</p>
+                    <BsBoxArrowUpRight />
+                </div>
+                <div className='flex items-center justify-center gap-2 rounded-xl px-7 py-3 bg-black/40' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}>
+                    <p>Contact Me</p>
+                    <MdMailOutline className='text-lg'/>
+                </div>
             </div>
-            <div>
-
+            <div className='flex text-2xl text-gray-100/60 gap-10'>
+                <a href="https://www.linkedin.com/in/kanujan-subakaran-156339292/" target='_blank' className='border p-2 rounded-xl' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}><FiGithub /></a>
+                <a href="https://github.com/KanujanS" target='_blank' className='border p-2 rounded-xl' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}><FiLinkedin /></a>
+                <a href="https://medium.com/@kanujansk" target='_blank' className='border p-2 rounded-xl' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}><FaMediumM /></a>
             </div>
         </div>
         <div>
