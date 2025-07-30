74cf8f06e04e3ce3159661a32649319ba815ed8e
KanujanS <kanujansk@gmail.com>
2025-07-30 22:51:24 +0530

    Home page updated

--- kanu-portfolio/src/App.jsx
+++ kanu-portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home'
 const App = () => {
   return (
     <div className='px-25 m-5'>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:24px_24px]"></div>
         <Navbar/>
         <Home/>
     </div>
--- kanu-portfolio/src/components/Home.jsx
+++ kanu-portfolio/src/components/Home.jsx
@@ -23,8 +23,8 @@ const Home = () => {
                 <span className='text-white text-2xl'>{text}</span>
                 <span className='text-sky-400 text-3xl animate-pulse ml-1'><Cursor cursorStyle='|'/></span>
             </p>
-            <p className='text-lg text-gray-400 py-5'>Enthusiastic and dedicated aspiring Software Engineer with a solid understanding of full-stack development.</p>
-            <div className='text-white text-sm space-x-5'>
+            <p className='text-lg text-gray-400 py-6'>Enthusiastic and dedicated aspiring Software Engineer with a solid understanding of full-stack development.</p>
+            <div className='text-white text-sm space-x-5 py-2'>
                 <button className='border border-gray-500 px-5 py-2 rounded-3xl bg-gray-100/10'>MongoDB</button>
                 <button className='border border-gray-500 px-5 py-2 rounded-3xl bg-gray-100/10'>Express.js</button>
                 <button className='border border-gray-500 px-5 py-2 rounded-3xl bg-gray-100/10'>React.js</button>
@@ -35,18 +35,18 @@ const Home = () => {
                     <p>Projects</p>
                     <BsBoxArrowUpRight />
                 </div>
-                <div className='flex items-center justify-center gap-2 rounded-xl px-7 py-3 bg-black/40' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}>
+                <div className='w-45 flex items-center justify-center gap-2 rounded-xl px-7 py-3 bg-black/40' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}>
                     <p>Contact Me</p>
                     <MdMailOutline className='text-lg'/>
                 </div>
             </div>
-            <div className='flex text-2xl text-gray-100/60 gap-10'>
+            <div className='flex text-2xl py-3 text-gray-100/60 gap-10'>
                 <a href="https://www.linkedin.com/in/kanujan-subakaran-156339292/" target='_blank' className='border p-2 rounded-xl' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}><FiGithub /></a>
                 <a href="https://github.com/KanujanS" target='_blank' className='border p-2 rounded-xl' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}><FiLinkedin /></a>
                 <a href="https://medium.com/@kanujansk" target='_blank' className='border p-2 rounded-xl' style={{ boxShadow: '0px 0px 20px rgba(14, 165, 233, 0.7)' }}><FaMediumM /></a>
             </div>
         </div>
-        <div>
+        <div className='flex justify-center items-center -mt-20 -mr-10'>
             <img src={PROFILE} alt="Profile Image" className='w-210'/>
         </div>
     </div>
