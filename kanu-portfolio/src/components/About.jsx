a1c4ec5cb8bf5c54fd9153f9510f8eb0c82e567b
KanujanS <kanujansk@gmail.com>
2025-08-01 23:23:31 +0530

    download cv button init

--- kanu-portfolio/src/components/About.jsx
+++ kanu-portfolio/src/components/About.jsx
@@ -9,8 +9,8 @@ const About = () => {
                 <p className='gradient-text-heading text-5xl font-bold mt-15'>Hello I'm,</p>
                 <p className='text-5xl font-bold my-5'>Kanujan Subakaran</p>
                 <p className='text-gray-400 text-lg'>Enthusiastic and dedicated aspiring Software Engineer with a solid understanding of full-stack development, particularly in the MERN stack <span className='font-medium text-gray-300'>(MongoDB, Express.js, React, Node.js)</span>. Passionate about building responsive web applications and continuously learning to stay updated with the latest software engineering practices.</p>
-                <div>
-                    <p></p>
+                <div className='my-3'>
+                    <p className='border '>Download CV</p>
                 </div>
             </div>
             <div className='w-1/2 text-white'>
