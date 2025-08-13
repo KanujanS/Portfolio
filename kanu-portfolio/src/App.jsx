e6af9790e220195008d7ee8e303301f984ee5a96
KanujanS <kanujansk@gmail.com>
2025-08-13 14:45:58 +0530

    Footer section finished

--- kanu-portfolio/src/App.jsx
+++ kanu-portfolio/src/App.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import About from './components/About'
-import Portfolio from './components/Portfolio'
-import TechStacks from './components/TechStacks'
-import Contact from './components/Contact'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import TechStacks from "./components/TechStacks";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className='px-25 m-5'>
-      <div className="bg-grid-full"></div>
-        <Navbar/>
-        <Home/>
-        <About/>
-        <Portfolio/>
-        <TechStacks/>
-        <Contact/>
+    <div>
+      <div className="px-25 m-5">
+        <div className="bg-grid-full"></div>
+        <Navbar />
+        <Home />
+        <About />
+        <Portfolio />
+        <TechStacks />
+        <Contact />
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ kanu-portfolio/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='pt-10 '>
+        <hr className='text-gray-400/50'/>
+        <div className='flex items-center justify-center text-gray-400 py-5'>
+            <p>&#169; 2025 Kanujan Subakaran. All Rights Reserved.</p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Footer
