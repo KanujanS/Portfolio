3dc1579d434d7f8ec5601ac046bac526b81e44b0
KanujanS <kanujansk@gmail.com>
2025-08-21 15:35:12 +0530

    menubar added for navbar

--- kanu-portfolio/src/components/Navbar.jsx
+++ kanu-portfolio/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import {Link} from 'react-scroll'
+import { HiMenu, HiX } from "react-icons/hi";
 
 
 const Navbar = () => {
   const [scrolled,setScrolled] = useState(false);
+  const [menuOpen,setMenuOpen] = useState(false);
   useEffect(() => {
     const handleScroll = ()=> {
       if (window.scrollY > 50) {
@@ -18,7 +20,7 @@ const Navbar = () => {
   }, [])
   
   return (
-    <div className={`fixed top-0 left-0 w-full z-50 px-7 py-5 md:px-30 ${scrolled ? 'bg-sky-950/20 backdrop-blur-xl shadow-md' : 'bg-transparent'}`}>
+    <div className={`fixed top-0 left-0 w-full z-50 px-7 py-5 md:px-30 ${menuOpen ? 'bg-black/80 backdrop-blur-3xl' : scrolled ? 'bg-sky-950/20 backdrop-blur-xl shadow-md' : 'bg-transparent'} `}>
       <div className='flex justify-between items-center'>
         <Link to='home' smooth={true} duration={500}>
            <p className='font-bold text-2xl bg-gradient-to-r from-sky-400 via-sky-600 to-sky-700 bg-clip-text text-transparent cursor-pointer'>KanujanSK</p>
@@ -42,8 +44,18 @@ const Navbar = () => {
                   <hr className='mt-1 border-none h-[1.5px] bg-white justify-center link-hr'/>
                 </li>
             </ul>
+            <button onClick={()=> setMenuOpen(!menuOpen)} className='md:hidden text-white text-3xl'>{menuOpen ? <HiX/> : <HiMenu/>}</button>
         </div>
       </div>
+      {menuOpen && (
+          <div className='md:hidden mt-5 p-5 space-y-5 text-white font-medium text-lg'>
+            {['home', 'about', 'portfolio', 'contact'].map((section)=>(
+              <Link key={section} to={section} smooth={true} duration={500} spy={true} offset={-100} activeClass='active-link' onClick={()=>setMenuOpen(false)} className='block cursor-pointer'>
+                {section.charAt(0).toUpperCase()+section.slice(1)}
+              </Link>
+            ))}
+          </div>
+        )}
     </div>
   )
 }
